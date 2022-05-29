6a431e6838cb5ba408d01e3e7ec592b21b85d782
Boris Mitov <boris.m.mitov@gmail.com>
2022-05-29 21:45:52 +0300

    Problem-Solving

---
+++ src/MultidimensionalArraysExercises/MatrixShuffling_05.java
@@ -0,0 +1,65 @@
+package MultidimensionalArraysExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatrixShuffling_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] dimensions = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+
+        String[][] matrix = new String[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            String[] arr = scanner.nextLine().split("\\s+");
+
+            matrix[row] = arr;
+        }
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("END")) {
+            String[] data = command.split("\\s+");
+            if (!data[0].equals("swap") && data.length != 5) {
+                System.out.println("Invalid input!");
+                command = scanner.nextLine();
+                continue;
+            }
+                int r1 = Integer.parseInt(data[1]);
+                int c1 = Integer.parseInt(data[2]);
+                int r2 = Integer.parseInt(data[3]);
+                int c2 = Integer.parseInt(data[4]);
+                if (r1 < 0 || r1 > matrix.length || c1 < 0 || c1 > matrix[0].length
+                        || r2 < 0 || r2 > matrix.length || c2 < 0 || c2 > matrix[0].length) {
+                    System.out.println("Invalid input!");
+                    command = scanner.nextLine();
+                    continue;
+                } else {
+                    String temp = matrix[r1][c1];
+                    matrix[r1][c1] = matrix[r2][c2];
+                    matrix[r2][c2] = temp;
+                    printMatrix(matrix);
+                }
+
+            command = scanner.nextLine();
+        }
+
+
+    }
+
+    private static void printMatrix(String[][] matrix) {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                System.out.print(matrix[r][c] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
--- src/MultidimensionalArraysExercises/MaximalSum_04.java
+++ src/MultidimensionalArraysExercises/MaximalSum_04.java
@@ -14,6 +14,49 @@ public class MaximalSum_04 {
 
         int[][] matrix = new int[rows][cols];
 
+        for (int row = 0; row < rows; row++) {
+            int[] arr = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
 
+            matrix[row] = arr;
+        }
+
+        int maxSum = Integer.MIN_VALUE;
+
+        int[][] result = new int[3][3];
+
+        for (int row = 0; row < rows - 2; row++) {
+            for (int col = 0; col < cols - 2; col++) {
+                int sum = matrix[row][col]
+                        + matrix[row][col + 1]
+                        + matrix[row][col + 2]
+                        + matrix[row + 1][col]
+                        + matrix[row + 1][col + 1]
+                        + matrix[row + 1][col + 2]
+                        + matrix[row + 2][col]
+                        + matrix[row + 2][col + 1]
+                        + matrix[row + 2][col + 2];
+
+                if (sum > maxSum) {
+                    maxSum = sum;
+                    result = new int[][]{
+                            {matrix[row][col], matrix[row][col + 1], matrix[row][col + 2]},
+                            {matrix[row + 1][col], matrix[row + 1][col + 1], matrix[row + 1][col + 2]},
+                            {matrix[row + 2][col], matrix[row + 2][col + 1], matrix[row + 2][col + 2]}
+                    };
+                }
+            }
+        }
+        System.out.printf("Sum = %d%n", maxSum);
+        printMatrix(result);
+    }
+    private static void printMatrix(int[][] matrix) {
+        for (int r = 0; r < matrix.length; r++) {
+            for (int c = 0; c < matrix[r].length; c++) {
+                System.out.print(matrix[r][c] + " ");
+            }
+            System.out.println();
+        }
     }
 }
