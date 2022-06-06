7d547d24f72bd172865434387b56c1cb295dbde7
Boris Mitov <boris.m.mitov@gmail.com>
2022-06-06 13:23:36 +0300

    Nested Folders, Find Even or Odds Problem-Solving

--- src/StreamsFilesАndDirectories/NestedFolders_08.java
+++ src/StreamsFilesАndDirectories/NestedFolders_08.java
@@ -2,10 +2,9 @@ package StreamsFilesАndDirectories;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class NestedFolders_08 {
     public static void main(String[] args) throws IOException {
@@ -15,11 +14,31 @@ public class NestedFolders_08 {
 
         File root = new File("Resources/Files-and-Streams");
 
-        System.out.println(Files.walk(root.toPath())
-                .map(Path::toFile)
-                .map(File::getName)
-                //.filter(File::isDirectory)
-                .collect(Collectors.joining(System.lineSeparator())));
+        Deque<File> dirs = new ArrayDeque<>();
+        dirs.offer(root);
+
+        int count = 0;
+
+        while (!dirs.isEmpty()) {
+            File current = dirs.poll();
+            File[] nestedFiles = current.listFiles();
+            for (File nestedFile : nestedFiles) {
+                if (nestedFile.isDirectory()) {
+                    dirs.offer(nestedFile);
+                }
+            }
+            count++;
+            System.out.println(current.getName());
+        }
+        System.out.println(count + " folders");
+
+//        System.out.println(Files.walk(root.toPath())
+//                .map(Path::toFile)
+//                .map(File::getName)
+//                //.filter(File::isDirectory)
+//                .collect(Collectors.joining(System.lineSeparator())));
+
+
 
 
     }
