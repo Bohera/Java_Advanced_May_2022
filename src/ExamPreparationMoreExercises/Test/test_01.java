650c9ee81debff06485b7426a6fe0d174b6618ac
Boris Mitov <boris.m.mitov@gmail.com>
2022-06-22 23:52:09 +0300

    Exam Preparation Exercise solving. Exam Retake April 2022

---
+++ src/ExamPreparationMoreExercises/Test/test_01.java
@@ -0,0 +1,9 @@
+package ExamPreparationMoreExercises.Test;
+
+import java.util.Scanner;
+
+public class test_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
