7615abf1580b882ee9ab0410a999770a1952efc4
Boris Mitov <boris.m.mitov@gmail.com>
2022-06-22 20:09:35 +0300

    Some generic exercises

---
+++ src/GenericsExercises/Threeuple/Tuple.java
@@ -0,0 +1,42 @@
+package GenericsExercises.Threeuple;
+
+public class Tuple<F, S, T> {
+    F first;
+    S second;
+    T third;
+
+    public Tuple(F first, S second, T third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public void setFirst(F first) {
+        this.first = first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public void setSecond(S second) {
+        this.second = second;
+    }
+
+    public T getThird() {
+        return third;
+    }
+
+    public void setThird(T third) {
+        this.third = third;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s -> %s -> %s", first, second, third);
+    }
+}
